9b57837814ad46ebe3bcead36ff7e3c966d1bcfb
xingliguang <707323795@qq.com>
2016-12-19 17:17:52 +0800

    微信秘钥设置

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
+﻿<?php
+require_once "jssdk.php";
+// appId  和 秘钥
+$jssdk = new JSSDK("wx71e48b63f9935c7d", "af46fbbc2828f60bfe1dd2bb59b7a660");
+$signPackage = $jssdk->GetSignPackage();
+?>
 <!doctype html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
 	<link rel="stylesheet" href="foot.css">
+
+<script type="text/javascript" src = "http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 	<meta content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" name="viewport" />  
 	 <script data-main = "app.js" type="text/javascript" src = "js/require.js"></script>
 </head>
@@ -42,4 +50,14 @@
 	</div>
 </body>
 <script src="js/public.js"></script>
+<script type="text/javascript">
+	 wx.config({
+    debug: true,
+    appId: '<?php echo $signPackage["appId"];?>',
+    timestamp: <?php echo $signPackage["timestamp"];?>,
+    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+    signature: '<?php echo $signPackage["signature"];?>',
+     jsApiList: []
+  });
+</script>
 </html>
