8eee805043fd75496a336521402d89c732f2e9bb
xingliguang <707323795@qq.com>
2016-12-20 11:13:15 +0800

    aaaaaa

--- modules/home/home.js
+++ modules/home/home.js
@@ -48,12 +48,13 @@ define(["text!./home.html","css!./home.css", "./swiper.min.js", "css!./swiper.mi
             });
 
 
-            var latitude=0;
-            var longitude=0;
+           
             $(".header1 span").click(function(){
             	getLocation();
-            	getOpen();
+            	
             });
+             var latitude=0;
+            var longitude=0;
             function getLocation(){
             	wx.getLocation({
 			    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
@@ -64,11 +65,8 @@ define(["text!./home.html","css!./home.css", "./swiper.min.js", "css!./swiper.mi
 			        var accuracy = res.accuracy; // 位置精度
 			        alert("纬度"+latitude);
 			        alert("经度"+longitude);
-			    }
-			});
-            };
-            function getOpen(){
-            	wx.openLocation({
+
+			        wx.openLocation({
 			    latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
 			    longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
 			    name: '', // 位置名
@@ -76,7 +74,12 @@ define(["text!./home.html","css!./home.css", "./swiper.min.js", "css!./swiper.mi
 			    scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
 			    infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
 			});
+
+
+			    }
+			});
             };
+          
             
 		}
 	}
