35a91ccc0e3f7f39965f40990e77d0ba5b4fa0f9
xingliguang <707323795@qq.com>
2016-12-20 11:02:43 +0800

    ass

--- modules/home/home.js
+++ modules/home/home.js
@@ -62,6 +62,8 @@ define(["text!./home.html","css!./home.css", "./swiper.min.js", "css!./swiper.mi
 			        longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
 			        var speed = res.speed; // 速度，以米/每秒计
 			        var accuracy = res.accuracy; // 位置精度
+			        alert("纬度"+latitude);
+			        alert("经度"+longitude);
 			    }
 			});
             };
